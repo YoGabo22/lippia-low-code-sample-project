0f59df47c6af03df77c369ace6a8e1974244e807
lgtmboop <alecarbonari99@gmail.com>
2025-04-25 11:11:56 -0300

    [LTM & CORE UPGRADED] Dependencies

---
+++ src/main/java/ar/reports/TestManagerReporter.java
@@ -0,0 +1,13 @@
+package ar.reports;
+
+import io.lippia.reporter.ltm.TestManagerAPIAdapter;
+
+public class TestManagerReporter extends TestManagerAPIAdapter {
+    public TestManagerReporter(String arg0) {
+    }
+
+    @Override
+    public String getBase64Image() {
+        return null;
+    }
+}
